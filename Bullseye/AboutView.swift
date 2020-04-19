5b3360781867a6a9218cc1c406eed54f6a98124d
dianamelga <di.melgarejo@gmail.com>
2020-04-19 11:10:23 -0400

    app using SwiftUI

---
+++ Bullseye/AboutView.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftUIView.swift
+//  Bullseye
+//
+//  Created by Diana Melgarejo on 4/19/20.
+//  Copyright © 2020 Diana Melgarejo. All rights reserved.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
