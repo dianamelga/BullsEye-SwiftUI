44113fdc5f4e53230f65710835dbf61bcc88bc63
dianamelga <di.melgarejo@gmail.com>
2020-04-19 11:10:34 -0400

    modifications

--- Bullseye/AboutView.swift
+++ Bullseye/AboutView.swift
@@ -1,5 +1,5 @@
 //
-//  SwiftUIView.swift
+//  AboutView.swift
 //  Bullseye
 //
 //  Created by Diana Melgarejo on 4/19/20.
@@ -8,14 +8,51 @@
 
 import SwiftUI
 
-struct SwiftUIView: View {
+struct AboutView: View {
+    struct HeaderTextStyle: ViewModifier {
+        func body(content: Content) -> some View {
+            return content
+            .foregroundColor(Color.black)
+                .padding(.bottom, 20)
+                .padding(.top, 20)
+            .font(Font.custom("Arial Rounded MT Bold", size: 30))
+            
+        }
+    }
+    
+    struct ContentTextStyle: ViewModifier {
+        func body(content: Content) -> some View {
+            return content
+            .foregroundColor(Color.black)
+                .padding(.bottom, 20)
+                .padding(.leading, 20)
+                .padding(.trailing, 20)
+            .font(Font.custom("Arial Rounded MT Bold", size: 16))
+            
+        }
+    }
+
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Group {
+        VStack {
+            Text("🎯 Bullseye 🎯").modifier(HeaderTextStyle())
+            Text("This is Bullseye, the game where you can win points and earn fame by dragging a slider.")
+                .modifier(ContentTextStyle())
+            Text("Your goal is to place the slider as close as possible to the target value. The closer you are, the more points you score.")
+            .modifier(ContentTextStyle())
+            Text("Enjoy!")
+            .modifier(ContentTextStyle())
+        }.background(Color(red: 255.0 / 255.0, green: 214.0 / 255.0, blue: 179.0 / 255.0))
+        .navigationBarTitle("About Bullseye")
+        }.background(Image("Background"))
     }
 }
 
-struct SwiftUIView_Previews: PreviewProvider {
+struct AboutView_Previews: PreviewProvider {
     static var previews: some View {
-        SwiftUIView()
+        AboutView()
+        .previewLayout(.fixed(width: 896, height: 414))
+        
     }
 }
--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -9,13 +9,178 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+    
+    @State var alertIsVisible: Bool = false
+    @State var sliderValue: Double = 50.0
+    @State var target: Int = Int.random(in: 1...100)
+    @State var score = 0
+    @State var round = 1
+    let midnightBlue = Color(red: 0.0 / 255.0, green: 51.0 / 255.0, blue: 102.0 / 255.0)
+    
+    struct LabelStyle: ViewModifier {
+        func body(content: Content) -> some View {
+            return content
+            .foregroundColor(Color.white)
+            .font(Font.custom("Arial Rounded MT Bold", size: 18))
+            .modifier(Shadow())
+        }
     }
+    
+    struct ValueStyle: ViewModifier {
+        func body(content: Content) -> some View {
+            return content
+                .foregroundColor(Color.yellow)
+                .font(Font.custom("Arial Rounded MT Bold", size: 24))
+                .modifier(Shadow())
+        }
+    }
+    
+    struct Shadow: ViewModifier {
+        func body(content: Content) -> some View {
+            return content
+                .shadow(color: Color.black, radius: 5, x: 2, y: 2)
+        }
+    }
+    
+    struct ButtonLargeTextStyle: ViewModifier {
+        func body(content: Content) -> some View {
+            return content
+                .foregroundColor(Color.white)
+                .font(Font.custom("Arial Rounded MT Bold", size: 18))
+                .modifier(Shadow())
+        }
+    }
+    
+    struct ButtonSmallTextStyle: ViewModifier {
+        func body(content: Content) -> some View {
+            return content
+                .foregroundColor(Color.white)
+                .font(Font.custom("Arial Rounded MT Bold", size: 12))
+                .modifier(Shadow())
+        }
+    }
+    
+    var body: some View {
+        
+        VStack {
+            Spacer()
+            // Target Row
+            HStack {
+                Text("Put the Bullseye as close as you can to:").modifier(LabelStyle())
+                
+                Text("\(target)").modifier(ValueStyle())
+                
+            }
+            Spacer()
+            // Slider Row
+            HStack {
+                Text("1").modifier(LabelStyle())
+                Slider(value: self.$sliderValue, in: 1...100).accentColor(Color.green)
+                Text("100").modifier(LabelStyle())
+            }
+            Spacer()
+            // Button row
+            Button(action: {
+                self.alertIsVisible = true
+                
+            }) {
+                Text("Hit me").modifier(ButtonLargeTextStyle())
+            }
+            .alert(isPresented: $alertIsVisible) {()-> Alert in
+                return Alert(
+                    title: Text("\(alertTitle())"),
+                    message: Text("The slider's value is \(sliderRoundedValue()).\n" +
+                        "You scored \(self.pointForCurrentRound()) points this round."),
+                    dismissButton: .default(Text("Awesome!")) {
+                        self.score += self.pointForCurrentRound()
+                        self.target = Int.random(in:1...100)
+                        self.round += 1
+                    })
+            }
+            .background(Image("Button"))
+            
+            Spacer()
+            // Score row
+            HStack {
+                Button(action: {
+                    self.startOver()
+                }) {
+                    HStack {
+                      Image("StartOverIcon")
+                      Text(/*@START_MENU_TOKEN@*/"Start Over"/*@END_MENU_TOKEN@*/).modifier(ButtonSmallTextStyle())
+                    }
+                }.background(Image("Button"))
+                Spacer()
+                Text(/*@START_MENU_TOKEN@*/"Score"/*@END_MENU_TOKEN@*/).modifier(LabelStyle())
+                Text("\(score)").modifier(ValueStyle())
+                Spacer()
+                Text("Round").modifier(LabelStyle())
+                Text("\(round)").modifier(ValueStyle())
+                Spacer()
+                NavigationLink(destination: AboutView()) {
+                    HStack {
+                        Image("InfoIcon")
+                    Text("Info").modifier(ButtonSmallTextStyle())
+                    }
+                }.background(Image("Button"))
+            }
+            .padding(.bottom, 20)
+            
+            
+            
+        }.background(Image("Background"), alignment: .center)
+        .accentColor(midnightBlue)
+        .navigationBarTitle("Bullseye")
+    }
+    
+    func amountOff() -> Int {
+        abs(sliderRoundedValue() - target)
+    }
+    
+    func pointForCurrentRound() -> Int {
+        var bonusPoints = 0
+        let points = 100 - amountOff()
+        
+        if(amountOff() == 0) {
+            bonusPoints = 100
+        }else if(amountOff() == 1) {
+            bonusPoints = 50
+        }
+        
+        return  points + bonusPoints
+    }
+    
+    func sliderRoundedValue() -> Int {
+        Int(sliderValue.rounded())
+    }
+    
+    func alertTitle() -> String {
+        let difference = amountOff()
+        let title: String
+        if(difference == 0) {
+            title = "Perfect!"
+        }else if (difference < 5) {
+            title = "You almost had it!"
+        }else if (difference <= 10) {
+            title = "Not bad."
+        }else {
+            title = "Are you even trying?"
+        }
+        
+        return title
+    }
+    
+    func startOver() {
+        score = 0
+        round = 1
+        target = Int.random(in:1...100)
+        sliderValue = 50.0
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewLayout(.fixed(width: 896, height: 414))
     }
 }
